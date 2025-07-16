8e7f1ab7d58c4f3fd507aa8de651849ab758c88b
Khadrawy <omarkhadrawy10@gmail.com>
2025-07-16 14:14:22 +0300

    Creating the Project Structure

---
+++ src/langgraphagenticai/main.py
@@ -0,0 +1,7 @@
+'''
+its used to call all the functions in the agenticai modules with different components from one place
+
+so we use to to connect all the components of the agenticai modules in one place
+
+to be called from the main function and to be used in the UI 
+'''
